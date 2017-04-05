a5fbbaaa78fa1bdd7e71f44e394a2215eba2af73
dorasaa <dorasaa@gmail.com>
2017-04-05 01:02:30 +0100

    first commit

---
+++ moq.tests/ICalculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moq.CalcBusiness
+{
+    public interface ICalculator
+    {
+        int Add(int param1, int param2);
+        int Subtract(int param1, int param2);
+        int Multipy(int param1, int param2);
+        int Divide(int param1, int param2);
+        int ConvertUSDtoCLP(int unit);
+    
+      }
+
+    public interface IUSD_CLP_ExchangeRateFeed
+    {
+        int GetActualUSDValue();
+    }
+
+
+    public class calc : ICalculator
+    {
+        private IUSD_CLP_ExchangeRateFeed _feed;
+        public calc(IUSD_CLP_ExchangeRateFeed  feed)
+        {          //constructor
+            this._feed = feed;
+        }
+
+        public int Add(int param1, int param2)
+        {
+            throw new NotImplementedException();
+        }
+        public int Subtract(int param1, int param2)
+        {
+            throw new NotImplementedException();
+        }
+        public int Multipy(int param1, int param2)
+        {
+            throw new NotImplementedException();
+        }
+        public int Divide(int param1, int param2)
+        {
+            return param1 / param2;
+        }
+
+        public int ConvertUSDtoCLP(int unit)
+        {
+            return unit * this._feed.GetActualUSDValue();
+        }
+
+    }
+}
---
+++ moq.tests/Moqtest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using moq.CalcBusiness;
+  
+
+namespace moq.tests
+{
+    [TestFixture]
+    public class Moqtest 
+    {
+        
+        public IUSD_CLP_ExchangeRateFeed prvActualUSDValue()
+        {
+            var mockIUSD_CLP_ExchangeRateFeed = new Mock<IUSD_CLP_ExchangeRateFeed>();  
+            mockIUSD_CLP_ExchangeRateFeed.Setup(m => m.GetActualUSDValue()).Returns(1000);
+            var objvalue = mockIUSD_CLP_ExchangeRateFeed.Object;
+            return objvalue;
+        }
+
+        [Test(Description ="Divide 9 by 3 ; expected result is 3")]
+        public void TC1_Divide9by3() {
+            IUSD_CLP_ExchangeRateFeed feed = this.prvActualUSDValue();
+            ICalculator calc1 = new calc(feed);
+            int res = calc1.Divide(9, 3);
+            Assert.AreEqual(3, res);
+            
+        }
+
+        [Test(Description = "Divide 9 by 0 ; dividebyzeroexception")]
+          public void TC1_Divide9by0()
+        {
+            IUSD_CLP_ExchangeRateFeed feed = this.prvActualUSDValue();
+            ICalculator calc1 = new calc(feed);
+
+            Assert.That(() => calc1.Divide(9, 0), Throws.TypeOf<DivideByZeroException>());   
+
+        }
+
+
+    }
+}
